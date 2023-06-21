ebb07748197b7dca03d2a542fc65d1d2207a5fc1
davibtorelli <davitorelli@outlook.com>
2023-06-21 09:45:28 -0300

    adicionando exemplos de todas as rotas possíveis

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/users/create', [UserController::class, 'create'])->name('user.create');
 Route::post('/users/create', [UserController::class, 'store'])->name('user.store');
 Route::get('/users', [UserController::class, 'index'])->name('user.index');
@@ -21,3 +22,19 @@ Route::get('/user/{id?}', [UserController::class, 'show'])->name('user.show');
 Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/user/update/{id}', [UserController::class, 'update'])->name('user.update');
 Route::delete('/users/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+//* Entendendo a estrutura das rotas.
+    // Route::get($uri, $callback);
+    // Route::post($uri, $callback);
+    // Route::put($uri, $callback);
+    // Route::patch($uri, $callback);
+    // Route::delete($uri, $callback);
+    // Route::options($uri, $callback);
+
+    // Route::match(['get', 'post'], '/', function () {
+    
+    // });
+    
+    // Route::any('/', function () {
+    
+    // });

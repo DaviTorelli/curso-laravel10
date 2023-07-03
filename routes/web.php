f09a3a9bef9f86dbac47f74b8c8b3dc1ceeee405
davibtorelli <davitorelli@outlook.com>
2023-07-03 14:19:25 -0300

    aplicando middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('users')->name('user.')->controller(UserController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/create', 'create')->name('create');
-    Route::get('/{user}', 'show')
-        ->missing(function () {
-            return redirect()->route('user.index');
-        })
-        ->name('show');
-    Route::post('/create', 'store')->name('store');
-    Route::get('/edit/{user}', 'edit')->name('edit');
-    Route::put('/update/{user}', 'update')->name('update');
-    Route::delete('/destroy/{user}', 'destroy')->name('destroy');
-});
+Route::prefix('users')->name('user.')->controller(UserController::class)
+    // ->middleware(MyFirstMiddleware::class) //Posso importar dessa forma caso não tenha declarado o Middleware no arquivo: Kernel.php
+    ->middleware('MyFirstMiddleware') 
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::get('/{user}', 'show')
+            ->missing(function () {
+                return redirect()->route('user.index');
+            })
+            ->name('show');
+        Route::post('/create', 'store')->name('store');
+        Route::get('/edit/{user}', 'edit')->name('edit');
+        Route::put('/update/{user}', 'update')->name('update');
+        Route::delete('/destroy/{user}', 'destroy')->name('destroy');
+    });
 
 Route::fallback(function () {
     return redirect()->route('user.index');
